e8be08ff3215f3a1ed13c3e255e9cc8a526d4a0b
shadid12 <shadidhaque2012@gmail.com>
2018-06-01 17:39:16 -0400

    added new react app

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// components
+import App from './App';
+
+//css
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
