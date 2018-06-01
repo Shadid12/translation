65bc326ba0eec666ac59bb42b8e89afb7d4e1ea3
shadid12 <shadidhaque2012@gmail.com>
2018-06-01 17:08:49 -0400

    adding new messages

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ import express from 'express';
 import {sync as globSync} from 'glob';
 import {readFileSync} from 'fs';
 import * as path from 'path';
+import serialize from 'serialize-javascript';
 
 const app = express()
 
+const translations = globSync('./lang/*.json')
+    .map((filename) => [
+        path.basename(filename, '.json'),
+        readFileSync(filename, 'utf8'),
+    ])
+    .map(([locale, file]) => [locale, JSON.parse(file)])
+    .reduce((collection, [locale, messages]) => {
+        collection[locale] = messages;
+        return collection;
+    }, {});
 
-const translations = globSync('./build/lang/*.json')
-    .map((filename) => {
-        path.basename(filename, '.json')
-    })
+app.get('/', (req, res) => {
+    let locale   = req.query.locale || 'en-UPPER';
+    let messages = translations[locale];
+    if (!messages) {
+        return res.status(404).send('Locale is not supported.');
+    }
+    res.send(`${serialize({locale, messages})}`)
+})
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
+// app.use(express.static('build'));
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
